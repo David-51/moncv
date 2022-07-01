a4bc01723d27aa31d0fd364102c9a06d79ba64c3
David GRIGNON <david@davidgrignon.com>
2022-07-02 00:11:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Je développe votre site internet ou votre applications pour attirer plus de clients. Parlez moi de votre projet ...">
+    <base href="./">
+    <link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <script async src="https://kit.fontawesome.com/3314838246.js" crossorigin="anonymous"></script>
+    
+    <title>La Guilde Média - Développement web</title>
+</head>
+<body>
+    <div>
+        <div >
+            <?php require_once './public/html/body.php'; ?>
+        
+            <footer class="container-fluid bg-primary text-light fixed-bottom">
+                <?php require_once './public/html/footer.php'; ?>
+            </footer>
+        </div>
+    </div>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ public/html/body.php
@@ -0,0 +1,43 @@
+
+    <!-- <div class="onglet"></div> -->
+    <div class="container">
+        <div class="row mt-5 mb-3">
+            <H1 class="text-center size-14">Je crée des sites simples et intuitifs pour attirer plus de clients</H1>
+        </div>
+        <div class="row profile m-auto">            
+            <img class="profile-picture m-0 p-0" src="./public/images/profile.png" alt="" srcset="">                        
+        </div>
+        <div class="row col m-auto">
+            <p class="text-center mt-2 mb-0 name">
+                David GRIGNON
+            </p>
+            <p class="text-center">
+                Développeur Web et Marketing
+            </p>
+            <h2 class="text-center">
+                Parlez-moi de votre projet maintenant
+            </h2>
+            <div class="col m-auto my-3 text-center">
+                <button class="btn btn-success btn-border m-auto p-2" data-bs-toggle="modal" data-bs-target="#contactform">
+                    cliquez ici&ensp;<i class="fa-solid fa-hand-pointer"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="contactform" tabindex="-1" aria-labelledby="formcontact" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="formcontaact">Parlez moi de votre projet ...</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            insérer formulaire ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>            
+        </div>
+        </div>
+    </div>
+    </div>
