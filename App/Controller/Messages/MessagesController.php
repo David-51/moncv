4ba34ac0e8804156f4ff94ce71ef3d7b195c4718
David GRIGNON <david@davidgrignon.com>
2022-07-27 00:06:21 +0200

    UPDATE Messages and .env.example to add Phpmailer informations

--- App/Controller/Messages/MessagesController.php
+++ App/Controller/Messages/MessagesController.php
@@ -12,7 +12,6 @@ if(!isset($_POST['firstname'], $_POST['lastname'], $_POST['subject'], $_POST['pr
     die;
 }
 
-echo json_encode(['Message' => $_POST['project']]);
 // Configure message to send by mail with phpmailer
 
 require 'vendor/phpmailer/phpmailer/src/Exception.php';
@@ -22,7 +21,6 @@ require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 require_once 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
-
 
 try {
     //Server settings
@@ -37,20 +35,17 @@ try {
 
     //Recipients
     $mail->setFrom($_ENV['PHPMAILER_FROM'], $_ENV['PHPMAILER_USERNAME']);
-    $mail->addAddress($_ENV['PHPMAILER_TO'], 'La Guilde');     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addAddress($_ENV['PHPMAILER_TO'], 'La Guilde');     //Add a recipient    
     $mail->addReplyTo($_ENV['PHPMAILER_FROM'], $_ENV['PHPMAILER_USERNAME']);
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $_POST['subject'];
     $mail->Body    = $_POST['project'];
-    // $mail->AltBody = $_POST['project'];
     
     $mail->send();
     echo 'Message has been sent';
+    Logger::setMessage('message has been sent subject :\''. $mail->Subject . '\' from : '. $_POST['email']);
 } catch (\Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     Logger::setMessage($mail->ErrorInfo);
